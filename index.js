7b1c0853ea272d5c127776d15fb64b95242f52c0
proy-mtc <partha@mentortechcollective.org>
2020-10-29 23:20:37 +0530

    2 files modified

--- index.js
+++ index.js
@@ -26,9 +26,20 @@ var guestbookListener = null;
 async function main() {
 
   // Add Firebase project configuration object here
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyDnLD-j5bbe-WjqiiE_tNbNcDmRnM2t_5E",
+    authDomain: "fir-web-codelab-85e9e.firebaseapp.com",
+    databaseURL: "https://fir-web-codelab-85e9e.firebaseio.com",
+    projectId: "fir-web-codelab-85e9e",
+    storageBucket: "fir-web-codelab-85e9e.appspot.com",
+    messagingSenderId: "1053608807404",
+    appId: "1:1053608807404:web:8548759f09011448a70ec9"
+  };
+
   // var firebaseConfig = {};
 
-  // firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
   // FirebaseUI config
   const uiConfig = {
@@ -46,7 +57,110 @@ async function main() {
     }
   };
 
-  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+  const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+   // Listen to RSVP button clicks
+ //    startRsvpButton.addEventListener("click",
+//      () => {
+ //         ui.start("#firebaseui-auth-container", uiConfig);
+ //     });
+// Called when the user clicks the RSVP button
+startRsvpButton.addEventListener("click",
+ () => {
+    if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+});
+
+// ...
+// Listen to the current Auth state
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user) => {
+ if (user){
+   startRsvpButton.textContent = "LOGOUT";
+   // Show guestbook to logged-in users
+   guestbookContainer.style.display = "block";
+
+     // Subscribe to the guestbook collection
+  subscribeGuestbook();
+
+ }
+ else{
+   startRsvpButton.textContent = "RSVP";
+   // Hide guestbook for non-logged-in users
+   guestbookContainer.style.display = "none";
+
+// Unsubscribe from the guestbook collection
+  unsubscribeGuestbook();
+
+ }
+});
+
+
+// Listen to the form submission
+form.addEventListener("submit", (e) => {
+ // Prevent the default form redirect
+ e.preventDefault();
+ // Write a new message to the database collection "guestbook"
+ firebase.firestore().collection("guestbook").add({
+   text: input.value,
+   timestamp: Date.now(),
+   name: firebase.auth().currentUser.displayName,
+   userId: firebase.auth().currentUser.uid
+ })
+ // clear message input field
+ input.value = ""; 
+ // Return false to avoid redirect
+ return false;
+});
+
+// ...
+// Create query for messages
+firebase.firestore().collection("guestbook")
+.orderBy("timestamp","desc")
+.onSnapshot((snaps) => {
+ // Reset page
+ guestbook.innerHTML = "";
+ // Loop through documents in database
+ snaps.forEach((doc) => {
+   // Create an HTML entry for each document and add it to the chat
+   const entry = document.createElement("p");
+   entry.textContent = doc.data().name + ": " + doc.data().text;
+   guestbook.appendChild(entry);
+ });
+});
+// Listen to guestbook updates
+function subscribeGuestbook(){
+   // Create query for messages
+ guestbookListener = firebase.firestore().collection("guestbook")
+ .orderBy("timestamp","desc")
+ .onSnapshot((snaps) => {
+   // Reset page
+   guestbook.innerHTML = "";
+   // Loop through documents in database
+   snaps.forEach((doc) => {
+     // Create an HTML entry for each document and add it to the chat
+     const entry = document.createElement("p");
+     entry.textContent = doc.data().name + ": " + doc.data().text;
+     guestbook.appendChild(entry);
+   });
+ });
+};
+
+// Unsubscribe from guestbook updates
+function unsubscribeGuestbook(){
+ if (guestbookListener != null)
+ {
+   guestbookListener();
+   guestbookListener = null;
+ }
+};
+
+
 }
 main();
 
